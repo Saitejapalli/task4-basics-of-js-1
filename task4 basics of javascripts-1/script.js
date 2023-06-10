8faaf5cac5356538f3f11738a33b0dea5aa46d0b
Saitejapalli <133526081+Saitejapalli@users.noreply.github.com>
2023-06-10 15:58:59 +0530

    Add files via upload

---
+++ task4 basics of javascripts-1/script.js
@@ -0,0 +1,82 @@
+console.log("hello guys");
+alert("welcome our world");
+
+//variables
+var numberone=1;
+console.log(numberone);
+
+let numbertwo=2;
+console.log(numbertwo);
+
+//constants
+const pi=3.1415;
+console.log(pi);
+
+//arithematic operations
+//addition
+let number1=10;
+let number2=5;
+result=(number1+number2);
+console.log(result);
+
+//substraction
+result1=(number1-number2);
+console.log(result1);
+
+//multiplication
+result2=(number1*number2);
+console.log(result2);
+
+//division
+result3=(number1/number2);
+console.log(result3);
+
+//reminder
+result4=(number1%number2);
+console.log(result4);
+
+//power/superscript
+result5=(number1**number2);
+console.log(result5);
+
+//increment
+result6=(number1++);
+console.log(number1);             //number1= 11
+
+//decrement
+result7=(number2--);
+console.log(number2);             //number2= 4
+
+//assignment operators
+let number3= 6 ;
+let number4= "6";
+//=  
+number3=6;
+console.log(number3);
+
+//==
+result8=(number3 == number4);
+console.log(result8);
+
+//===
+result9=(number3===number4);
+console.log(result9);
+
+//+=
+result10=(number2+=7);
+console.log(number2);              //number2= 11
+
+//!=
+result11=(number1!=number2);
+console.log(result11);             // false
+
+//-=(subtracting the variable)
+result12=(number1 -= number2);
+console.log(result12);              //number1-number2=0
+
+
+//*=(multiplication the variable)
+result13= (number3*=2);
+console.log(result13);
+
+
